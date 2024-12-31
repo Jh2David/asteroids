87f7ebbaca0868a2071dcd7d9ee581e41fcf27d5
Jehan DAVID <jehan.david@pm.me>
2024-12-31 18:49:44 +0100

    add circle in our screen

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import pygame
 
 from constants import *
+from circleshape import CircleShape
 
 
 def main():
     pygame.init()
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    circle = CircleShape(x=100, y=100, radius=30)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             clock.tick(60)
+
+        circle.update(dt)
         screen.fill((0, 0, 0))
+        circle.draw(screen)
         pygame.display.flip()
-        dt += clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
