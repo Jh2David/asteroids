81d5100c1193f210a8b5c43a1b2b243b18fd6262
Jehan DAVID <jehan.david@pm.me>
2024-12-31 18:03:18 +0100

    clean main.py (black & isort)

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -15,6 +12,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
