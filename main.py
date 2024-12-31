fe6970cfb31321ab2d865cf1225ffdeb9d2826d3
Jehan DAVID <jehan.david@pm.me>
2024-12-31 22:00:46 +0100

    add method asteroid split

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
             for shot in shots:
                 if asteroid.check_collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         # Draw
         screen.fill((0, 0, 0))
